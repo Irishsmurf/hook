7edb0af57c9cc0d8d6680ea3f0dbe366fc68f629
Emerson Loureiro <emerson.loureiro@gmail.com>
2014-04-04 22:17:23 +0100

    NO-ISSUE:
    . Added license information across the board.

--- src/main/java/cf/janga/hook/core/Extension.java
+++ src/main/java/cf/janga/hook/core/Extension.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Emerson Loureiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *    Emerson Loureiro - initial API, implementation, and documentation
+ *******************************************************************************/
 package cf.janga.hook.core;
 
 /**
@@ -24,13 +35,14 @@ public interface Extension<T extends CoreAPI> {
 
 	/**
 	 * Initializes this extension. This method is automatically called by the
-	 * {@link SimplePluginPlatform} whenever the plugin associated with the extension
-	 * is loaded, and the extension initialized.
+	 * {@link SimplePluginPlatform} whenever the plugin associated with the
+	 * extension is loaded, and the extension initialized.
 	 * 
-	 * @param coreAPI The {@link CoreAPI} of the host application this extension
-	 *            is associated to.
-	 * @throws PluginException If there's any error while initializing this
-	 *             extension point.
+	 * @param coreAPI
+	 *            The {@link CoreAPI} of the host application this extension is
+	 *            associated to.
+	 * @throws PluginException
+	 *             If there's any error while initializing this extension point.
 	 */
 	void init(T coreAPI) throws PluginException;
 }
--- src/main/java/cf/janga/hook/core/PlatformException.java
+++ src/main/java/cf/janga/hook/core/PlatformException.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Emerson Loureiro.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *    Emerson Loureiro - initial API, implementation, and documentation
+ *******************************************************************************/
 package cf.janga.hook.core;
 
 /**
@@ -13,7 +24,8 @@ public class PlatformException extends Exception {
 	 * Creates a new {@code PlatformException} having the provided throwable as
 	 * the cause.
 	 * 
-	 * @param cause The root cause of this exception.
+	 * @param cause
+	 *            The root cause of this exception.
 	 */
 	public PlatformException(final Throwable cause) {
 		super(cause.getMessage(), cause);
